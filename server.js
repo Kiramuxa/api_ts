3c9f9a04fd0c7f320711d2f4f55281ea945c1817
Kirill Mikheev <master.salesman88@gmail.com>
2017-11-25 21:04:42 +0300

    port change to 80

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = 80;
 require('./routes')(app, {});
 
 app.listen(port, function() {
